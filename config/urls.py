5acdfe1b63fc1c91254d705ae23ac0cb3b7ad24d
Vitalii Omelai <v.omelay@gmail.com>
2025-05-05 16:15:33 +0300

    Added project

---
+++ config/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+from apps.posts.urls import Posts
+from apps.proxy.urls import Proxy
+
+
+pages = [
+    path('admin/', admin.site.urls),
+]
+
+endpoints = [
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/posts/', include((Posts.endpoints, 'posts'), namespace='posts')),
+    path('api/v1/proxy/', include((Proxy.endpoints, 'proxy'), namespace='proxy')),
+]
+
+urlpatterns = (
+    []
+    + pages
+    + endpoints
+)
